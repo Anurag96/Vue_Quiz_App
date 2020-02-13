0a4ca6907c705f4d8283e660ec766f952fbf36cf
anuragkumar96 <s531372@nwmissouri.edu>
2020-02-13 12:31:45 -0600

    Adding header and Question Component

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+         <b-nav tabs>
+    <b-nav-item disabled>Fancy Quiz App</b-nav-item>
+    <b-nav-item>disabled: 4/10</b-nav-item>
+
+  </b-nav>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="question-box-container">
+    <b-jumbotron>
+         <template v-slot:lead>
+      Some question here
+    </template>
+
+    <hr class="my-4">
+
+    <p>
+      list of anwser
+    </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button variant="success" href="#">next</b-button>
+    </b-jumbotron>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
